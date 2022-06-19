cfaf4df22821d3835a763e0fd095f097e6d39bbc
raborodin <anton.borodin91@gmail.com>
2022-06-19 18:36:37 +0300

    created a blog app

--- functional_tests.py
+++ functional_tests.py
@@ -10,10 +10,19 @@ class BasicInstallTest(unittest.TestCase):
         self.browser.quit()
 
     def test_install(self):  
+        # В браузере открылся сайт (по адресу: 'http://127.0.0.1:8000')
+        # В заголовке сайта прочитали "Сайт Алексея Куличевского"
 
         self.browser.get('http://127.0.0.1:8000')
-        self.assertIn('Congratulations!', self.browser.title)
-        #self.fail('Finish the test!')
+        self.assertIn('Сайт Алексея Куличевского', self.browser.title)
+
+    def test_home_page_header(self):
+        # В шапке сайта написно "Алексей Куличевский"
+        browser = self.browser.get('http://127.0.0.1:8000')
+        header = browser.find_elements_by_tag_name('h1')[0]
+        self.assertIn('Сайт Алексея Куличевского', self.browser.title)
 
 if __name__ == '__main__':  
     unittest.main()
+
+# self.fail('Finish test!')
